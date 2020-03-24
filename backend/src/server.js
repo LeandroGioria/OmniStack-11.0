96dfaa98f5119f364eeccb8cecde39c38344813a
Leandro Gioria <legioria@hotmail.com>
2020-03-24 20:33:52 -0300

    configured knex to use sqlite3

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('', (request, response) => {
-  return response.send('Hello World!');
+app.get('', (req, res) => {
+  return res.send('Hello World!');
 });
 
 app.listen(3333);
