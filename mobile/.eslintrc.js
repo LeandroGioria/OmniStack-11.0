bc54793ccc07bb6d4f2ba5bfb5aeefc855657a6f
Leandro Gioria <legioria@hotmail.com>
2020-03-27 21:13:03 -0300

    configured eslint and prettier

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
   env: {
     es6: true,
-    jest: true,
-    browser: true
   },
   extends: [
-        'airbnb',
-        'prettier',
-        'prettier/react'
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-base',
+   		 	'prettier',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly'
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -25,36 +24,36 @@ module.exports = {
     'react',
     'prettier',
     'react-hooks',
-    'import-helpers',
+    'import',
     'jsx-a11y'
   ],
   rules: {
-        "prettier/prettier": "error",
-        "react/jsx-filename-extension": ['error', { extensions: ['.jsx', '.js'] }],
-        "import/prefer-default-export": 'off',
-        "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-        "react/jsx-one-expression-per-line": "off",
-        "global-require": "off",
-        "react-native/no-raw-text": "off",
-        "no-param-reassign": "off",
-        "no-underscore-dangle": "off",
-        camelcase: "off",
-        "no-console": ["error", { allow: ["tron"] }],
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        'import-helpers/order-imports': [
-          'warn',
-          {
-              newlinesBetween: 'always', // new line between groups
-              groups: [
-                  '/^react/',
-                  'module',
-                  '/^@shared/',
-                  ['parent', 'sibling', 'index'],
-              ],
-              alphabetize: { order: 'asc', ignoreCase: true },
-          },
-      ],
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ['error', { extensions: ['.jsx', '.js'] }],
+    "import/prefer-default-export": 'off',
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "react/jsx-one-expression-per-line": "off",
+    "global-require": "off",
+    "react-native/no-raw-text": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    camelcase: "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    'import-helpers/order-imports': [
+      'warn',
+      {
+          newlinesBetween: 'always', // new line between groups
+          groups: [
+              '/^react/',
+              'module',
+              '/^@shared/',
+              ['parent', 'sibling', 'index'],
+          ],
+          alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
   },
   settings: {
     "import/resolver": {
