4838d4785b949da258abe4aef5e5311702ddbddc
Leandro Gioria <legioria@hotmail.com>
2020-03-29 14:31:13 -0300

    Implemented first integration test

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+const {errors} = require('celebrate');
+
+const app = express();
+
+app.use(cors(
+    // {
+    // origin: 'http://meuapp.com'
+    // }
+));
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,3 @@
-const express = require('express');
-const routes = require('./routes');
-const cors = require('cors');
-const {errors} = require('celebrate');
-
-const app = express();
-
-app.use(cors(
-    // {
-    // origin: 'http://meuapp.com'
-    // }
-));
-app.use(express.json());
-app.use(routes);
-app.use(errors());
+const app = require('./app');
 
 app.listen(3333);
